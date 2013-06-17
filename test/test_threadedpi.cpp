0d8f067c9095c0c05221886d68f1a0e4d2f4e567
Ryan McKinnon <ryanmmckinnon@gmail.com>
2013-06-16 20:23:21 -0400

    Addition of license.

--- test/test_threadedpi.cpp
+++ test/test_threadedpi.cpp
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2013  Ryan McKinnon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <cmath>
 
 #include "pisimulation.hpp"
@@ -26,8 +43,9 @@ BOOST_AUTO_TEST_CASE(ExpectedPiApproximationManySamples)
     p2();
 
     double pi = p2.get_pi();
+    double epsilon = 0.05;
 
-    BOOST_CHECK((pi < 3.2) && (pi > 3.1));
+    BOOST_CHECK(std::abs(pi - 3.1415) < epsilon);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
