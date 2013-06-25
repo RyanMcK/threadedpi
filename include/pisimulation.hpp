653ff821029141afe97d38cf953d806bdcf0c1f8
Ryan McKinnon <ryanmmckinnon@gmail.com>
2013-06-24 21:10:44 -0400

    Formatting changes.

--- include/pisimulation.hpp
+++ include/pisimulation.hpp
@@ -1,60 +1,44 @@
-/*
-Copyright (C) 2013  Ryan McKinnon
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Copyright (C) 2013  Ryan McKinnon
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef PISIMULATION_HPP
 #define PISIMULATION_HPP
 
 #include <random>
 
-/**
-@mainpage
+/// @mainpage Please use the menu above to view available classes and files.
 
-Please use the menu above to view available classes and files.
-*/
-
-/**
-Self-contained class for approximating pi in a finite number of Monte Carlo
-samples.
-*/
+/// Self-contained class for approximating pi in a finite number of Monte Carlo
+/// samples.
 class PiSimulation
 {
 public:
-    /**
-    @param samples Number of Monte Carlo samples to perform.
-    */
+    /// @param samples Number of Monte Carlo samples to perform.
     PiSimulation(int samples);
 
-    /**
-    Actually perform the Monte Carlo routine using the number of samples
-    provided during construction.
-    @see sample()
-    */
+    /// Actually perform the Monte Carlo routine using the number of samples
+    /// provided during construction.
+    /// @see sample()
     void operator()();
 
-    /**
-    @return The approximation of pi obtained via sampling.
-    */
+    /// @return The approximation of pi obtained via sampling.
     double get_pi();
 
 private:
-    /**
-    Pick a random point in the unit square and determine whether it lies in
-    the unit circle.  Store the results.
-    */
+    /// Pick a random point in the unit square and determine whether it lies in
+    /// the unit circle.  Store the results.
     void sample();
 
     int samples_;
